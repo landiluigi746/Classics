528da615e9ec9469478cc013aa541aaed262ebed
landiluigi746 <landiluigi865@gmail.com>
2024-07-29 14:25:10 +0200

    Project Reset

--- include/common/commons.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef CLASSICS_COMMONS_HPP
-#define CLASSICS_COMMONS_HPP
-
-#include <common/utils.hpp>
-#include <common/program.hpp>
-#include <common/gameobject.hpp>
-
-#endif // !CLASSICS_COMMONS_HPP
